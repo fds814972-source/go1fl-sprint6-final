e69bedaaa3d7c273c5ad6bc110940fdeb7c56024
Stepan Gapeev <L.T.L.Y.L@ya.ru>
2026-04-27 04:25:15 +0500

    first commit

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,32 @@
 package service
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func AutoConvertation(data string) (string, error) {
+	td := strings.TrimSpace(data)
+	if len(td) == 0 {
+		return "", fmt.Errorf("Empty line")
+	}
+
+	isMorse := true
+	// функция определения строки или морзе с конвертацией
+	for _, r := range td {
+		if r != '.' && r != '-' && r != ' ' {
+			isMorse = false
+			break
+		}
+	}
+
+	if isMorse {
+		td = morse.ToText(td)
+	} else {
+		td = morse.ToMorse(td)
+	}
+
+	return td, nil
+}
